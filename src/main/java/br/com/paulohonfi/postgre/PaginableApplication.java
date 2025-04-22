10e03db8d5c3bbe8ecc8adaea7054aa0cb7e5352
Paulo Honfi <contato@paulohonfi.com.br>
2025-04-21 23:30:18 -0300

    Initial commit

---
+++ src/main/java/br/com/paulohonfi/postgre/PaginableApplication.java
@@ -0,0 +1,13 @@
+package br.com.paulohonfi.postgre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/paulohonfi/postgre/PaginableApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.paulohonfi.postgre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
